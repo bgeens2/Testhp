a0ae86093c6faf97a4f73b8ec41bacdaa7834630
bgeens2 <bg@ipng.be>
2020-04-10 18:23:52 +0200

    Swr
    Swr

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./styles.css";
 import QRCode from "react-qr-code";
+import useSWR from "swr";
 
 export default function App() {
+  const { data, error } = useSWR("/api/user", fetch);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
   return (
     <div className="App">
       <h1>Hotspot app</h1>
